e86acb8f32ec95d007123d9d94f1d4ac92aba8f0
nattolecats <34679325+nattolecats@users.noreply.github.com>
2024-12-04 20:53:34 +0530

    ivan:Set display saturation to 0.95

--- device.mk
+++ device.mk
@@ -9,5 +9,9 @@ DEVICE_PATH := device/oplus/ivan
 # Inherit from ossi device
 $(call inherit-product, device/oplus/mt6877-common/common.mk)
 
+# Display saturation adjust
+PRODUCT_VENDOR_PROPERTIES += \
+    persist.sys.sf.color_saturation=0.95
+
 # Inherit from vendor blobs
 $(call inherit-product, vendor/oplus/ivan/ivan-vendor.mk)
