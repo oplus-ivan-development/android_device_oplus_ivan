88821be099b30b8105361031af46dc5335f719a0
Notganesh <chimeports@gmail.com>
2024-12-20 22:15:50 +0530

    ivan:dropped untracked blobs

--- extract-files.sh
+++ extract-files.sh
@@ -27,7 +27,7 @@ if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
 
 ANDROID_ROOT="${MY_DIR}"/../../..
 
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh
+HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
 if [ ! -f "${HELPER}" ]; then
     echo "Unable to find helper script at ${HELPER}"
     exit 1
@@ -36,56 +36,26 @@ source "${HELPER}"
 
 function blob_fixup {
     case "$1" in
-        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so)
-            "$PATCHELF" --replace-needed libutils.so libutils_v32.so "$2"
-            ;;
-        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek|vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
-	    [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libcodec2_hidl@1.0.so" "${2}"
-            "${PATCHELF}" --add-needed "libshim.so" "${2}"
-            "${PATCHELF}" --add-needed "libstagefright_foundation_v33.so" "${2}"
+        vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so|\
+        vendor/lib*/libmtkcam_stdutils.so)
+            "$PATCHELF" --replace-needed libutils.so libutils-v32.so "$2"
             ;;
         vendor/bin/mtk_agpsd)
            "$PATCHELF" --replace-needed libcrypto.so libcrypto-v32.so "$2"
-           "$PATCHELF" --replace-needed libssl.so libssl-v32.so "${2}"
             ;;
-        vendor/lib*/libkeystore-engine-wifi-hidl.so)
-            "$PATCHELF" --replace-needed android.system.keystore2-V1-ndk_platform.so android.system.keystore2-V1-ndk.so "$2"
+        vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
+            grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             ;;
-        vendor/lib64/libmtkcam_featurepolicy.so)
-            # evaluateCaptureConfiguration()
-            sed -i "s/\x34\xE8\x87\x40\xB9/\x34\x28\x02\x80\x52/" "$2"
+        odm/lib*/libui_oplus.so)
+            "$PATCHELF" --replace-needed android.hardware.graphics.common-V2-ndk_platform.so android.hardware.graphics.common-V2-ndk.so "$2"
             ;;
-        vendor/bin/hw/android.hardware.thermal@2.0-service.mtk)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+        vendor/lib*/libmtkisp_metadata.so)
+            "${PATCHELF}" --replace-needed "libui.so" "libui_oplus.so" "${2}"
             ;;
-        vendor/bin/hw/android.hardware.wifi@1.0-service-lazy)
-            "${PATCHELF}" --replace-needed "libwifi-hal.so" "libwifi-hal-mtk.so" "${2}"
+	    vendor/etc/init/android.hardware.neuralnetworks@1.3-service-mtk-neuron.rc)
+            sed -i 's/start/enable/' "$2"
             ;;
-        lib64/libem_support_jni.so)
-            "${PATCHELF}" --add-needed "libjni_shim.so" "${2}"
-            ;;
-        vendor/lib64/hw/sensors.mt6893.so)
-            "${PATCHELF}" --add-needed "libsensors_shim.so" "${2}"
-            ;;
-        vendor/bin/mnld)
-            ;&
-        vendor/lib64/libaalservice.so)
-            "${PATCHELF}" --replace-needed "libsensorndkbridge.so" "libsensorndkbridge-v30.so" "${2}"
-            ;;
-        vendor/lib64/libsource.so)
-            grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
-            ;;
-        system/lib64/libsource.so)
-            grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
-            ;;
-        system_ext/lib64/libsource.so)
-            grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
-            ;;
-        vendor/lib/libcodec2_vndk-mtk.so)
-            grep -q libshim_ui.so "$2" || "$PATCHELF" --add-needed libshim_ui.so "$2"
-            ;;
-    esac
+  esac
 }
 
 # Default to sanitizing the vendor folder before extraction
